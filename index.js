02f125ec25ca09c58683de0580e3b2c069ab564f
Christine Arroyo <chrstinearroyo@gmail.com>
2024-06-27 11:19:43 +0800

    javascript

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 //Eyy maalam na ahaha//
-console.log('Hi po');
+console.log('Hello World');
 
 function myFunction() {
     document.getElementById("demo").innerHTML = 
     "Oh Nooo Fa-keeee ahahahha";
 }
+
+let a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+document.getElementById("demo1").innerHTML = c;
 
