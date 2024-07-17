4109aa0c44c02e8edf3d99e9680953a20465732a
Christine Arroyo <chrstinearroyo@gmail.com>
2024-07-17 15:51:27 +0800

    Delete index2.js

--- index2.js
+++
@@ -1,5 +0,0 @@
-const temp = [30,35,40,45,50,55,60];
-let high = temp.findLast(x => x > 45);
-document.getElementById("demo19").innerHTML = "The last temperature was over " + high;
-
-console.log("hello bitch");
